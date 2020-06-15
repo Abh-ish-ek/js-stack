45775fae64e740361f2f41b70f2c8c4a07838e7c
Abhishek Gupta <abhishek@indusaction.org>
2020-06-16 01:17:36 +0530

    class and constructor

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
-let stackObj = {
-  data: [],
-  push: function (x) {
-    return this.data.push(x);
-  },
-  pop: function () {
-    return this.data.pop();
-  },
-};
+// My module
+// function stackObj(x) {
+//   data: [], (this.data = x);
+// }
 
-module.exports = {
-  stackObj,
-};
+// stackObj.prototype.push = function pushData() {
+//   return this.data.push(this.data);
+// };
+// stackObj.prototype.pop = function popData() {
+//   return this.data.pop();
+// };
+
+class stackObj {
+  constructor(input) {
+    data: [], (this.input = input);
+  }
+  push() {
+    return this.data.push(x);
+  }
+
+  pop() {
+    return this.data.pop();
+  }
+}
+
+module.exports = stackObj;
+
+// stackObj = {
+//   data: [],
+//   push: function (x) {
+//     return this.data.push(x);
+//   },
+
+//   pop: function () {
+//     return this.data.pop();
+//   },
+// };
+// // module.exports['stkObj'] = stkObj;
--- test.js
+++ test.js
@@ -1,11 +1,14 @@
-const { stackObj } = require('./index');
+const { stackObj, stackObj1 } = require('./index');
 const testPush = function (res) {
   try {
     stackObj.push(5);
     stackObj.push('Abhishek');
     stackObj.push(5);
-    stackObj.pop();
-  } catch (err) {}
+    stackObj1.pop(6);
+    console.log(stackObj);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 testPush();
